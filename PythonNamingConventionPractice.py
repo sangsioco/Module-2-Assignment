5ead7e8be0de8f13f5df9636b7e6aa5cae9bc3f0
sangsioco <sangsioco@gmail.com>
2024-05-11 11:43:39 -0700

    adding task for python convention practice

---
+++ PythonNamingConventionPractice.py
@@ -0,0 +1,15 @@
+# Task 1 Code Correction
+
+# This is the original value
+
+Pi_value = 3.14
+userAge = 25
+user_location = "New York"
+MAXLIMIT = 1000
+
+# This is the corrected value
+
+pi_value = 3.14
+user_age = 25
+user_location = "New York"
+max_limit = 1000

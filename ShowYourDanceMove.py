3262110986d096f193eee7024821c6fbf2fe75a8
sangsioco <sangsioco@gmail.com>
2024-05-11 11:36:22 -0700

    updating showyourdancemove

--- ShowYourDanceMove.py
+++ ShowYourDanceMove.py
@@ -6,3 +6,17 @@ if weather == "sunny":
     print("Wear sunglasses!")
 else:
     print("Take an umbrella!")
+
+
+# Task 2
+
+# Ask the user how they feel today
+
+user_feeling = input("How are you feeling today? ")
+
+# Check the user's response and respond accordingly
+
+if user_feeling == "happy":
+    print("That's great to hear!")
+elif user_feeling == "sad":
+    print("I hope your day gets better!")

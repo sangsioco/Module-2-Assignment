2c5a1e91f962921a832b2b9b3c9212e624ba474c
sangsioco <sangsioco@gmail.com>
2024-05-11 12:08:53 -0700

    adding value swapping description

---
+++ AssignmentComparisons.py
@@ -0,0 +1,12 @@
+# Task 1: value Swapping
+
+# Given numbers
+a = 10
+b = 20
+
+# Swap the values using assignment operators
+a, b = b, a
+
+# Compare to ensure they have been swapped
+print("a after swapping:", a)  # Should print 20
+print("b after swapping:", b)  # Should print 10

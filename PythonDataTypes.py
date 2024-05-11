846f322da0e849e33af8254c799f924ad085edce
sangsioco <sangsioco@gmail.com>
2024-05-11 11:49:11 -0700

    adding python data types

---
+++ PythonDataTypes.py
@@ -0,0 +1,12 @@
+# Task 1
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print("Data type of variable_a:", type(variable_a))
+print("Data type of variable_b:", type(variable_b))
+print("Data type of variable_c:", type(variable_c))
+print("Data type of variable_d:", type(variable_d))
+

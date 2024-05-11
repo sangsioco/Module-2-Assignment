c3ca6c20018f40b38df961d1da96f96b0d6cf7d7
sangsioco <sangsioco@gmail.com>
2024-05-11 12:03:49 -0700

    adding description on aritmetic operation

---
+++ ArithmeticOperation.py
@@ -0,0 +1,18 @@
+# Task 1: Grocery Store Math
+
+dozen_egg = 4.99
+gallon_milk = 2.50
+bread = 1.75
+
+total = dozen_egg + gallon_milk + bread
+print("Total Cost:", total)
+
+# Task 2: Bank Interest
+
+initial_amount = 10000  # Initial amount in dollars
+interest_rate = 0.07    # Interest rate (7% expressed as a decimal)
+
+# Calculate the total amount after a year
+total_amount = initial_amount + (initial_amount * interest_rate)
+
+print("Total amount after a year: $", total_amount)
